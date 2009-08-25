a311d989da6d2406f774e1ce83fe077971a694c0
Don Johnson <don.johnson@peopleadmin.com>
2009-08-24 23:41:27 -0500

    My First Checkin, awwwwwe....

--- javascripts/counter.js
+++ javascripts/counter.js
@@ -1,12 +1,15 @@
-
 $(document).ready( function() {
   $('#tweetbox').keyup(function(){ update_character_count($(this)); });
+  var value = $.url.param("txt");
+  if ((value) && (value != "")) {
+    $('#tweetbox:input').val($.base64Decode(value));
+  } 
   update_character_count($('#tweetbox'));
   
   $('#save').click(function() {
     count = $('#count').text();
     tweet = $('#tweetbox').val();
-    $('#drafts ul').prepend("<li><a href='#'>" + tweet + "</a></li>");
+    $('#drafts ul').prepend("<li><a href='#'>" + tweet + "</a><span id=\"remove_tweet\" class=\"right ui-icon ui-icon-close\" title=\"Remove Tweet\"></span><a href=\""+ gen_url($('#tweetbox:input').val()) +"\" target=\"_blank\"><span id=\"perma_link\" class=\"right ui-icon ui-icon-extlink\" title=\"Permalink to this Tweet\"></span></a></li>");
     return false;
   });
   
@@ -15,7 +18,25 @@ $(document).ready( function() {
     update_character_count($('#tweetbox'));
     return false;  
   });
+
+  $('#perma_link').live('click', function() { 
+    window.open(this.parent().href);
+    return false;  
+  });
+
+  $('#remove_tweet').live('click', function() { 
+    drop_tweet($(this));
+    return false;  
+  });
+
+  $("#sorted_drafts").sortable();
 });
+
+function gen_url(input) {
+  var text = $.base64Encode(input);
+  var link = "/?txt="+text
+	return link;
+}
 
 function update_character_count(clicked) {
 	var length = clicked.val().length;
@@ -26,4 +47,9 @@ function update_character_count(clicked) {
 	  $('#count').removeClass('negative');
 	}
   $('#count').html(count);
+}
+
+function drop_tweet(clicked) {
+	var dad = clicked.parent();
+	dad.remove();
 }
